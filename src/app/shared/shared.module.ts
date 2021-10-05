7fd3e3de2bc3d5966382b066ccef838ba6440ff8
Ignacio Jelvez <ijelvezh@gmail.com>
2021-10-05 15:44:29 -0300

    Validaciones edicion

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,20 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
-
-
+import { RouterModule } from '@angular/router';
+import { AppRoutingModule } from '../app-routing.module';
 
 @NgModule({
-  declarations: [
-    FooterComponent,
-    HeaderComponent
-  ],
-  exports:[
-    FooterComponent,
-    HeaderComponent
-  ],
-  imports: [
-    CommonModule
-  ]
+  declarations: [FooterComponent, HeaderComponent],
+  exports: [FooterComponent, HeaderComponent],
+  imports: [CommonModule, RouterModule, AppRoutingModule],
 })
-export class SharedModule { }
+export class SharedModule {}
